e60f996fd6b04df1b38cb09ccb682498683d939f
James Pennell <pennell.james@gmail.com>
2012-09-12 19:47:54 -0400

    Add copy public key alias

--- system/aliases.sh
+++ system/aliases.sh
@@ -4,6 +4,9 @@ alias ls='ls -l'
 #cd
 alias ..="cd .."
 
+#Keys
+alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
+
 #Misc
 alias e=$EDITOR
 alias reload='. ~/.bash_profile'
--- system/config.sh
+++ system/config.sh
@@ -1,2 +1,14 @@
 export CLICOLOR=1
 export EDITOR='sublime'
+
+# GRC colorizes nifty unix tools all over the place
+if $(grc &>/dev/null)
+then
+  source `brew --prefix`/etc/grc.bashrc
+fi
+
+#History
+export HISTSIZE=32768
+export HISTFILESIZE=$HISTSIZE
+export HISTCONTROL=ignoredups
+export HISTIGNORE="ls:cd:cd -:pwd:exit:date:* --help"
