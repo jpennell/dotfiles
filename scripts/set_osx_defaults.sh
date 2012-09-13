0face32e54c12c9f570ddb9176f4fc91971f0459
James Pennell <pennell.james@gmail.com>
2012-09-12 21:12:29 -0400

    Mission control animations

--- scripts/set_osx_defaults.sh
+++ scripts/set_osx_defaults.sh
@@ -201,3 +201,5 @@ defaults write com.apple.TextEdit RichText -int 0
 defaults write com.apple.TextEdit PlainTextEncoding -int 4
 defaults write com.apple.TextEdit PlainTextEncodingForWrite -int 4
 
+#Speed up mission control animations
+defaults write com.apple.dock expose-animation-duration -float 0.15
