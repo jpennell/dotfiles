95ab801b35bd71981420dfbe01fdf12a3b5e0363
James Pennell <pennell.james@gmail.com>
2012-12-03 00:16:39 -0500

    Add mysqlstatus alias

--- mysql/aliases.sh
+++ mysql/aliases.sh
@@ -1,3 +1,4 @@
 alias mysqlstart='/usr/local/Cellar/mysql/5.5.27/bin/mysql.server start'
 alias mysqlstop='/usr/local/Cellar/mysql/5.5.27/bin/mysql.server stop'
 alias mysqlrestart='/usr/local/Cellar/mysql/5.5.27/bin/mysql.server restart'
+alias mysqlstatus='/usr/local/Cellar/mysql/5.5.27/bin/mysql.server status'
